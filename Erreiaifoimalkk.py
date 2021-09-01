b3781ee49938dbad09071370cea328cbd67125a5
Klayk8bit <89755796+Klayk8bit@users.noreply.github.com>
2021-09-01 12:51:52 -0300

    Create Erreiaifoimalkk.py

---
+++ Erreiaifoimalkk.py
@@ -0,0 +1,121 @@
+print('\033[35m'+'FALA')
+import time
+import os
+import webbrowser
+url = 'https://youtu.be/LVMHYzVS9Y4'
+oito = 8
+frase = 1000
+dois = 2
+select = 1000
+pms = 3
+user = 'Qual teu nome?'
+os.system('clear')
+print('        \n\n\n\n  Fala ai meu xapa...\n\n')
+time.sleep(3)
+print('_________________________________________')
+
+user = input('Qual é teu nome mn?' '\033[36m' + '\n-')
+time.sleep(2)
+print('\n\n.            prepara pra o textao kk')
+time.sleep(2)
+print('\n\n Carai kk nome fei da poha kkkkkkkk zoas te amo kk')
+pms = input('Essa ferramenta é apenas para auxiliar seus usuarios com links que certas vezes eles querem e n sabem onde achar. Resumindo essa ferramenta, ela vem com alguns links todos seguros que vc pode usar para seus prezeres pessoais e etc. \nEssa ferramenta tbm foi feita por um programador mto fajuto kkkk que so sabe o basico em python kajajajjakj mto otario ele kkkkkkk ainda bem q n é ele q ta escrevendo isso kakakakak... Dor......... Essa ferramenta nao ta bem programada ent se vc quiser modificar ela fica a vontade. \n E é isso kkk. \n\n\n\n Vc entendeu oque eu quis dizer, {} ? \n \033[35m' + '(1)Sim ademer, eu te amo\n(2)Não kk q merda'.format(user))
+if pms == 1:
+    time.sleep(2)
+    print('\n\n\nQue bom mn, seja feliz ai kk\n\n\n')
+if pms == 2:
+    time.sleep(2)
+    print('\n\n.    taaaahhhh.  \n\n\n\n')
+time.sleep(1)
+os.system('clear')
+
+print('\n\n Bem vindo {} \n '.format(user))
+time.sleep(4)
+print('.    \033[34m' + '    KLAYK 1.0.                       ')
+print('\033[36m' + '__________________________________________')
+print('|    - Escolhe um ai meu ¬' '\033[34m' + '  @Klayk16      |')
+print('|       \033[36m' + '                                  |')
+print('| 10 - Ferranentas termux mto picas kk.   |') 
+print('| 20 - Minecraft de gratis                |')
+print('| 30 - Como hackear wifi com termux.      |')
+print('| 40 - Frasezinha pra alegrar seu dia ^-^ |')
+print('| 50 - Grupos de whatsapp.                |')
+print('| 60 - Terraria gratis.                   |')
+print('| 70 - Melhor sensi do freefas kk         |')
+print('| 80 - 18 + hehehe                        |')
+print('| 90 - Apps pra programar pelo celular.   |')
+print('| 100 - Jogos com mod menu.               |')
+print('| 110 - Nostalgia.                        |')
+print('——————————————————————————————————————————')
+
+select = int(input('----:'))
+if select == 10:
+    print('Todas as ferramentas q vc, vagabundo da net que joga freefire, precisa kk \n \n \n Ddos ataque: \n\n https://github.com/cyweb/hammer \n\nSocialSploit : \n\n  https://github.com/Cesar-Hack-Gray/SocialSploit \n\n FSociety : \n\n  https://github.com/Manisso/fsociety \n\n Kiny painel pra fazer umas consulta ai kk : \n\n https://github.com/Kiny-Kiny/Kiny-Painel \n\n Spam do whatsapp : \n\n https://github.com/sandiwijayani1/SpamWa \n Ferramenta com ferramentas kk: \n\n https://github.com/DeepSociety/&wiki-termux \n\n Outra ferramenta com ferramenta ;-; : \n \n https://github.com/Helexiel/HeleScript \n\nOutra outra ferramenta com ferramentas kkkk: \n\n https://github.com/Zian25/UniTools-Termux \n\n\n\n.   Por enquanto é so isso ne kkk eu podia fazer um comando pra instalar os scripts de maneira automatica mas vai assim memo kk Versao 1.5 eu faço isso :)')
+if select == 20:
+    print('.      Copia e cola no google.     \n\n Minecraft 1.14 \n\n https://www.mediafire.com/download/2qujozckabi59oz \n\n Minecraft 1.15 \n\n https://www.mediafire.com/download/sjw5g9m1fkd74b3 \n\n Minecraft 1.16 \n\n https://www.mediafire.com/download/2lj4vd0m1v4m48e \n\n Minecraft 1.17 \n\n https://www.mediafire.com/download/h1fxg0mcrpzhzcs \n\n É isso ai kk')
+if select == 30:
+    os.system('clear')
+    time.sleep(3)
+    print(' \n\n\n.      Isso da cadeia, irmão.\n\n')
+    time.sleep(3)
+    os.system('clear')
+    print('\n\n\n.      Eu quero e sempre quis o seu bem...')
+    time.sleep(3)
+    os.system('clear')
+    print('\n\n\n.       Somos melhores que isso')
+    time.sleep(3)
+if select == 40:
+    print('\n\n\nSe o mundo for ruim com vc, meu amigo, xingue a mãe dele kkk\n\n\n')
+if select == 50:
+    print('\n\n\n https://gruposwhats.app/ \n\n\n')
+if select == 60:
+    print('\n\nTerraria 1.4.0.5\n\n\nhttps://www.mediafire.com/download/bfh5d27e68u88xm\n\n')
+if select == 70:
+    print('\n\n\n Ent meu caro kk A sensi mais utilizada e a melhor é: \n\n')
+    time.sleep(5)
+    print('\n\nGeral - 90 \nRed dot - 70 \nMira2x - 65 \nMira4x - 65\nMira AWM - 20 \nOLhadinha - 65 \n\n (N jogo freefire kk)')
+if select == 80:
+    os.system('clear')
+    print('\nXvideo.com\nxnxx.com\nsuperhentais.com\nnhentai.net\n\n kk so os mais famoso\n\n')
+    time.sleep(3)
+    print('P')
+    time.sleep(1)
+    print('u')
+    time.sleep(1)
+    print('n')
+    time.sleep(1)
+    print('h')
+    time.sleep(1)
+    print('e')
+    time.sleep(1)
+    print('t')
+    time.sleep(1)
+    print('e')
+    time.sleep(1)
+    print('r')
+    time.sleep(1)
+    print('o')
+    time.sleep(1)
+    print(' ')
+    time.sleep(1)
+    print('S')
+    time.sleep(1)
+    print('a')
+    time.sleep(1)
+    print('f')
+    time.sleep(1)
+    print('a')
+    time.sleep(1)
+    print('d')
+    time.sleep(1)
+    print('o')
+    time.sleep(3)
+if select == 90:
+    time.sleep(3)
+    os.system('clear')
+    time.sleep(1)
+    print(' Qpython \n\nhttps://play.google.com/store/apps/details?id=org.qpython.qpy3\n\n Dcoder \n\n https://play.google.com/store/apps/details?id=com.paprbit.dcoder \n\n pydroid \n\n https://play.google.com/store/apps/details?id=ru.iiec.pydroid3 \n\n (So os pica kk) ')
+if select == 100:
+    print('\nMELHOR APP PRA BAIXAR APPS COM APK MODIFICADO E DE GRAÇA! \n\n\nhttps://pt.happymod.com/')
+if select == 110:
+    print('\n.       Apenas... Abra... \n\n\n.   https://youtu.be/LVMHYzVS9Y4')
